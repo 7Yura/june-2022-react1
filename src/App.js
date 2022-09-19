1c1a9894ac3f67b7a74cc93f0c7cb2beee12eba1
7Yura <svido777@gmail.com>
2022-09-19 15:00:46 +0300

    char1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
-import './App.css';
-import Users from "./components/users/Users";
-import UserChoser from "./components/user-choser/UserChoser";
+import Characters from "./components/Characters/Characters";
 
 function App() {
 
 
   return (<div>
-    <h2>app component header</h2>
-    {/*<Users/>*/}
 
-    <UserChoser/>
+    <Characters/>
 
   </div>);
 }
 
 export default App;
+
