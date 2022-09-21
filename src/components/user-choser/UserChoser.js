f7e6b2a41a6ff0db217d0333af05caed320dccb1
7Yura <svido777@gmail.com>
2022-09-21 21:51:13 +0300

    user

--- src/components/user-choser/UserChoser.js
+++ src/components/user-choser/UserChoser.js
@@ -1,11 +1,10 @@
 import {useEffect, useState} from "react";
-import {getUserAxios, getUserPosts} from "../../services/user.api.axios.service";
+import {getUserAxios} from "../../services/user.api.axios.service";
 
 export default function UserChoser() {
 
 
     let [user, setUser] = useState(null);
-    let [post, setPost] = useState(null);
     let [id, setId] = useState(1);
 
 
@@ -24,16 +23,13 @@ export default function UserChoser() {
         getUserAxios(id).then(({data}) => {
             setUser(data);
         })
-        getUserPosts(id).then(({data}) => {
-            setPost(data);
-        })
     }, [id]);
 
 
     return (<div>
 
         <div>{JSON.stringify(user)}</div>
-        {/*<div>{JSON.stringify(post)}</div>*/}
+
         <button onClick={() => {
             decrement(id)
         }}>prev
