6bca61fa46c7de31ccce73a581d2dde09faeffb7
7Yura <svido777@gmail.com>
2022-09-27 14:59:29 +0300

    todo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import './App.css';
-// import Users from "./components/users/Users";
-// import UserChoser from "./components/user-choser/UserChoser";
 import {
     Route,
     Routes,
@@ -8,7 +6,6 @@ import {
 } from "react-router-dom";
 
 import {Home} from "./components/home/Home";
-// import Users from "./components/users/Users";
 import {Todos} from "./components/todos/Todos";
 import {Comments} from "./components/comments/Comments";
 import PostDetails from "./components/postDetails/PostDetails";
@@ -17,20 +14,21 @@ function App() {
 
 
   return (<div>
-
+<h3>Menu</h3>
     <ul>
       <li><Link to={'/'}>Home page</Link></li>
-      <li><Link to={'/todos page'}>todos page</Link></li>
-      <li><Link to={'/comments page'}>comments page</Link></li>
+      <li><Link to={'/todos'}>todos page</Link></li>
+      <li><Link to={'/comments'}>comments page</Link></li>
     </ul>
 <hr/>
+      <h3> Content</h3>
     <Routes>
 
       <Route path={'/'} element={<Home/>}/>
       {/*<Route index element={<Home/>}/>*/}
-      <Route path={'todos page'} element={<Todos/>}/>
-      {/*<Route path={'/albums page'} element={}/>*/}
-      <Route path={'comments page'} element={<Comments/>}>
+      <Route path={'todos'} element={<Todos/>}/>
+      {/*<Route path={'/albums'} element={}/>*/}
+      <Route path={'comments'} element={<Comments/>}>
         <Route path={':id'} element={<PostDetails/>}/>
     </Route>
     </Routes>
--- src/components/todos/Todos.js
+++ src/components/todos/Todos.js
@@ -1,7 +1,6 @@
 import React, { useEffect,useState} from 'react';
+
 import {Todo} from "../todo/Todo";
-
-
 import {getTodoAxios} from "../../services/todo.api.service";
 
 
